3925e826946f3b1d34140ed18dc1565a900ed1f8
Markidon <86127865+Markidon@users.noreply.github.com>
2021-06-26 18:23:15 +0300

    Add files via upload

---
+++ Homework№2.1/Homework№2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите минимальную температуру от -30°С до 0°С");
+            int min_temaperatura = Convert.ToInt32(Console.ReadLine());  
+
+            Console.WriteLine("Ведите максимальную температуру от +1°С до +36°С");
+            int max_temaperatura = Convert.ToInt32(Console.ReadLine());
+            int srednay_temperarura = (min_temaperatura+ max_temaperatura)/2;
+
+                if (min_temaperatura >= -30 && min_temaperatura <= 0 && max_temaperatura >= 1 && max_temaperatura<=36)
+                {
+                    Console.WriteLine($"Минимальная температура - {min_temaperatura}°С ");
+                    Console.WriteLine($"Максимальная температура - +{max_temaperatura}°С ");
+                    if(srednay_temperarura < 0)
+                    { 
+                    Console.WriteLine($"Средняя температура - {srednay_temperarura}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Средняя температура - +{srednay_temperarura}");
+                    }
+                }
+                else
+                {
+                Console.WriteLine("Вы превысили максимальную или минимальную температуру");
+                }
+
+        }
+    }
+}
