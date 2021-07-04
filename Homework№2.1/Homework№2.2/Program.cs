e31aabca2eeee8e932dbb268cb31f4a0a96563a5
Markidon <86127865+Markidon@users.noreply.github.com>
2021-07-05 02:11:19 +0300

    Add files via upload

---
+++ Homework№2.1/Homework№2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2._2
+{
+    // Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              
+        }
+    }
+}
