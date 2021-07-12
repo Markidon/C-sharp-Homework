8ecae2da806b16f3912db0a1e142de3f3c9be040
Markidon <86127865+Markidon@users.noreply.github.com>
2021-07-12 21:11:50 +0300

    Add files via upload

---
+++ Homework№1/Homework№1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name} сегодняшняе время {DateTime.Now}");
+
+
+        }
+    }
+}
---
+++ Homework№2/Homework№2.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2._2
+{
+    // Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите порядковый номер месяца от 1 до 12: ");
+
+            bool namberMonth = Int32.TryParse(Console.ReadLine(), out int month);
+            ProverkaNamberMonth(month);
+            Console.WriteLine("Программа завершена ");
+
+            Console.ReadKey();
+        }
+        static int ProverkaNamberMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+
+                Console.WriteLine($"заебись месяц {month} это: {(Season)month} ");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Введите от 1 до 12");
+                Console.Write("Ведите еще раз: ");
+                while (!Int32.TryParse(Console.ReadLine(), out month))
+                {
+                }
+                if (month >= 1 && month <= 12)
+                {
+                    Console.WriteLine($"заебись месяц {month} это: {(Season)month} ");
+                }
+                else
+                {
+                    Console.WriteLine("Нет такого месяца");
+                }
+            }
+            return month;
+
+        }
+    }   
+ }
+
+
